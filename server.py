ad368c3656f4d35224baf4972a1fcb6e4fffc801
Felipe <rdrgues@hotmail.com>
2022-01-25 21:58:48 -0700

    first batch of progress

--- server.py
+++ server.py
@@ -30,9 +30,69 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(1024).decode('utf-8')
+        req = self.data.split(' ')
+        print("method:", req[0])
+        print("file:", req[1])
+
+        method = req[0]
+        file_requested = req[1]
+
+        if method == "GET":
+            if file_requested == "/":
+                file_requested = "/index.html"
+
+            # print(req[1].lstrip("/"))
+            # raise Exception
+
+            # file = open(req[1].lstrip("/"), "rb")
+
+            try:
+                file_name = "www" + file_requested
+                file = open(file_name, "rb")
+                response = file.read()
+                file.close()
+
+                header = "HTTP/1.1 200 OK\n"
+
+                if file_name.endswith(".css"):
+                    mimetype = "text/css"
+                else:
+                    mimetype = "text/html"
+
+                header += 'Content-Type: '+str(mimetype)+'\n\n'
+            except Exception as e:
+                header = "HTTP/1.1 404 Not Found\n\n"
+                response = 'HTTP/1.1 404 Not Found'.encode('utf-8')
+
+            final_response = header.encode("utf-8")
+            # print(type(final_response))
+            # print(type(response))
+            # raise Exception
+            final_response += response
+
+            # print(header)
+            # print(response)
+            # raise Exception
+
+            print(final_response)
+
+            # final_response = final_response.encode("utf-8")
+
+            self.request.sendall(final_response)
+
+            # print ("Got a request of: %s\n" % self.data)
+            # self.request.sendall(bytearray("OK la concha",'utf-8'))
+
+        else:
+            header = "HTTP/1.1 405 Method Not Allowed\n\n"
+
+            final_response = header.encode("utf-8")
+
+            print(final_response)
+            self.request.sendall(final_response)
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
